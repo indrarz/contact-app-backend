6d4ec762869a8a4d13bc993254127661063bde20
Indra Rizky Gunawan <indra_rz@apps.ipb.ac.id>
2023-10-09 14:30:06 +0700

    Add CORS

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactModule } from './contact/contact.module';
+import { NestFactory } from '@nestjs/core';
 
 @Module({
   imports: [ContactModule],
@@ -9,3 +10,18 @@ import { ContactModule } from './contact/contact.module';
   providers: [AppService],
 })
 export class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Mengaktifkan CORS
+  app.enableCors({
+    origin: 'https://contact-apps.up.railway.app/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(3000);
+}
+
+bootstrap();
